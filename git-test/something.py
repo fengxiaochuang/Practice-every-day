1482819cd27481aa82a80c0bb06f567def665c10
lanxinxichen@126.com <lanxinxichen@126.com>
2017-04-16 21:34:54 +0800

    练习git

---
+++ git-test/something.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/4/16 21:32
+# @Author  : XIAOCHUANG
+# @Site    : www.fengxiaochuang.com/ 
+# @File    : something.py
+# @Software: PyCharm
+print("something is ready")
